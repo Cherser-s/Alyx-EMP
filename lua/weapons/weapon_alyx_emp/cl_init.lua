c889d5191be0e2c96de98feea66722c8b02b9894
Cherser-s <serg.cherniadjev@gmail.com>
2017-08-05 21:51:52 +0400

    Optimized clientside view and world models, fixed error with bone manipulation when weapon is dropped.

--- lua/weapons/weapon_alyx_emp/cl_init.lua
+++ lua/weapons/weapon_alyx_emp/cl_init.lua
@@ -12,38 +12,36 @@ util.PrecacheModel("models/alyx_emptool_prop.mdl")
 function SWEP:Initialize()
   self:SetHoldType(self.HoldType)
   local EMPModel=ClientsideModel("models/alyx_emptool_prop.mdl")
-  local EMPModel2=ClientsideModel("models/alyx_emptool_prop.mdl")
-  self:SetNoDraw(true)
 
-  self.EMP=EMPModel --view model
-  self.EMPW=EMPModel2 -- world model
-  local vm = self.Owner:GetViewModel()
-  if IsValid(vm) then
-    self:ResetView(vm)
-  end
+  self.EMP=EMPModel --model
+	if IsValid(self.Owner) then
+	  local vm = self.Owner:GetViewModel()
+	  if IsValid(vm) then
+	    self:ResetView(vm)
+		else
+			self:UpdateWorldModel()
+		end
+		--open it
+		self.EMP:SetSequence(2)
+	else
+		--stay closed
+		self.EMP:SetSequence(1)
+	end
   self.EMP:SetNoDraw(true) --will draw it in Draw()
   self.EMP:SetParent(self)
-  self.EMPW:SetNoDraw(true) --will draw it in Draw()
-  self.EMPW:SetParent(self)
-
-  self:UpdateWorldModel()
-
   self.EMP:Spawn()
-  self.EMPW:Spawn()
-  self.EMP:SetSequence(2)
-  self.EMPW:SetSequence(2)
 end
 
 
 local material=Material("cable/blue_elec")
 function SWEP:DrawWorldModel()
   self:UpdateWorldModel()
-  self.EMPW:DrawModel()
+  self.EMP:DrawModel()
   if self:GetFiring() then
     local trace=util.TraceLine(util.GetPlayerTrace(self.Owner))
     if trace.Hit then
       render.SetMaterial(material)
-      render.DrawBeam( self.EMPW:LocalToWorld(Vector(4,0,0)), trace.HitPos, 5, -math.random(0.3,1),math.random(0.3,1) , Color(255,255,255,180))
+      render.DrawBeam( self.EMP:LocalToWorld(Vector(4,0,0)), trace.HitPos, 5, -math.random(0.3,1),math.random(0.3,1) , Color(255,255,255,180))
     end
   end
 end
@@ -67,7 +65,6 @@ end
 
 function SWEP:Deploy()
   self.EMP:SetSequence(2)
-  self.EMPW:SetSequence(2)
   local vm = self.Owner:GetViewModel()
   if IsValid(vm) then
     self:ResetView(vm)
@@ -76,12 +73,19 @@ function SWEP:Deploy()
 end
 
 function SWEP:UpdateWorldModel()
-  local PosW,AngW=self.Owner:GetBonePosition(self.Owner:LookupBone("ValveBiped.Bip01_R_Hand")) //world model
-  self.EMPW:SetPos(PosW+AngW:Right()*(self.WElements.el1.pos.y)+AngW:Forward()*(self.WElements.el1.pos.x)+AngW:Up()*(self.WElements.el1.pos.z))
-  AngW:RotateAroundAxis(AngW:Up(),self.WElements.el1.angle.y)
-  AngW:RotateAroundAxis(AngW:Right(),self.WElements.el1.angle.p)
-  AngW:RotateAroundAxis(AngW:Forward(),self.WElements.el1.angle.r)
-  self.EMPW:SetAngles(AngW)
+	--dropped
+	if not IsValid(self.Owner) then
+		self.EMP:SetPos(self:GetPos())
+		self.EMP:SetAngles(self:GetAngles())
+	else
+		--holding
+	  local PosW,AngW=self.Owner:GetBonePosition(self.Owner:LookupBone("ValveBiped.Bip01_R_Hand")) //world model
+	  self.EMP:SetPos(PosW+AngW:Right()*(self.WElements.el1.pos.y)+AngW:Forward()*(self.WElements.el1.pos.x)+AngW:Up()*(self.WElements.el1.pos.z))
+	  AngW:RotateAroundAxis(AngW:Up(),self.WElements.el1.angle.y)
+	  AngW:RotateAroundAxis(AngW:Right(),self.WElements.el1.angle.p)
+	  AngW:RotateAroundAxis(AngW:Forward(),self.WElements.el1.angle.r)
+	  self.EMP:SetAngles(AngW)
+	end
 end
 
 function SWEP:ResetView(vm)
@@ -105,13 +109,9 @@ function SWEP:OnRemove()
   if  self.EMP:IsValid() then
     self.EMP:Remove()
   end
-  if  self.EMPW:IsValid() then
-    self.EMPW:Remove()
-  end
 end
 
 function SWEP:Holster(wep)
   self.EMP:SetSequence(1)
-  self.EMPW:SetSequence(1)
   return true
 end
